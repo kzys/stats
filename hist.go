63d620dd5d7c52a7113ead591e59524f3f6d41db
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2021-11-16 07:25:56 -0800

    Print a plain histogram

--- hist.go
+++ hist.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func printHistogram(values []float64, min float64, max float64) {
 	binCount := 10
@@ -8,14 +11,23 @@ func printHistogram(values []float64, min float64, max float64) {
 	binSize := (max - min) / float64(binCount)
 	end := binSize
 	var index int
+	var maxBin int
 	for _, v := range values {
 		if v > end {
-			bins = append(bins, 0)
 			end += binSize
 			index++
 			continue
 		}
 		bins[index]++
+		if bins[index] > maxBin {
+			maxBin = bins[index]
+		}
 	}
-	fmt.Printf("bins = %v, binSize = %f\n", bins, binSize)
+
+	fmt.Printf("\nhistogram (bins=%d)\n", binCount)
+
+	for _, v := range bins {
+		length := v / maxBin * 70
+		fmt.Printf("%3d: %s\n", v, strings.Repeat(".", length))
+	}
 }
